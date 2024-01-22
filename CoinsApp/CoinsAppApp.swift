3e43db1787bfa38f081eac5cbeba6a1796aeb21f
Almayo Ibrahim <almayo.online@gmail.com>
2024-01-22 14:02:53 +0300

    Initial Commit

---
+++ CoinsApp/CoinsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsAppApp.swift
+//  CoinsApp
+//
+//  Created by almayo ibrahim  on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
